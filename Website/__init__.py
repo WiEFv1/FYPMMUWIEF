8c7d2ca9cdca609dee02eaf1c877605abed883d9
WiEFv1 <w13f2406@gmail.com>
2024-05-10 00:22:01 +0800

    Made a working version of the website

--- Website/__init__.py
+++ Website/__init__.py
@@ -4,5 +4,12 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'testTestestTest'
     
+    from .view import view
+    from .auth import auth
+    
+    app.register_blueprint(view, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
+    
     return app
     
