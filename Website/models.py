c206c532408ac0fd4323fbd4a8efe12f50f952da
WiEFv1 <w13f2406@gmail.com>
2024-05-17 07:43:11 +0800

    Added Tables that can be added

--- Website/models.py
+++ Website/models.py
@@ -2,7 +2,6 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
@@ -10,23 +9,16 @@ class Note(db.Model):
     shared_user = db.Column(db.Integer, db.ForeignKey('user.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note', foreign_keys='Note.user_id')
-    taskU = db.relationship('Task', foreign_keys='Task.user_id')
-    commentU = db.relationship('Comment', foreign_keys='Comment.user_id')
-    projTableU = db.relationship('Projecttable', foreign_keys='Projecttable.user_id')
-'''   
-class Notification(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_sender = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user_reciever = db.Column(db.Integer, db.ForeignKey('user.id'))
-    note_dataNotif = db.Column(db.Integer, db.ForeignKey('note.id'))
-'''
+    tasks = db.relationship('Task', foreign_keys='Task.user_id')
+    comments = db.relationship('Comment', foreign_keys='Comment.user_id')
+    project_tables = db.relationship('Projecttable', foreign_keys='Projecttable.user_id')
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(10000))
@@ -35,18 +27,18 @@ class Task(db.Model):
     description = db.Column(db.String(10000))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     comment_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
-    prota_id = db.Column(db.Integer, db.ForeignKey('projecttable.id')) 
+    projecttable_id = db.Column(db.Integer, db.ForeignKey('projecttable.id'))
+    comments = db.relationship('Comment', backref='task', foreign_keys='Comment.task_id')
 
 class Projecttable(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(10000))  
     description = db.Column(db.String(10000))  
-
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    taskPT = db.relationship('Task',foreign_keys='Task.prota_id')            
+    tasks = db.relationship('Task', backref='projecttable', foreign_keys='Task.projecttable_id')
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     comment_detail = db.Column(db.String(10000))
-    taskC = db.relationship('Task', foreign_keys='Task.comment_id')     
+    task_id = db.Column(db.Integer, db.ForeignKey('task.id'))
--- Website/views.py
+++ Website/views.py
@@ -1,32 +1,38 @@
+# views.py
+
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, Projecttable
 from . import db
 import json
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
+        if 'add_note' in request.form:
+            note = request.form.get('note')  # Gets the note from the HTML
+            if len(note) < 1:
+                flash('Note is too short!', category='error')
+            else:
+                new_note = Note(data=note, user_id=current_user.id)  # Providing the schema for the note
+                db.session.add(new_note)  # Adding the note to the database
+                db.session.commit()
+                flash('Note added!', category='success')
+        
+        if 'create_table' in request.form:  # Check if the create table form is submitted
+            new_projecttable = Projecttable(user_id=current_user.id, name="Untitled Table", description="Insert Description Here")
+            db.session.add(new_projecttable)
             db.session.commit()
-            flash('Note added!', category='success')
+            flash('Table Added!', category='success')
 
     return render_template("home.html", user=current_user)
 
-
 @views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+def delete_note():
+    note = json.loads(request.data)  # This function expects a JSON from the INDEX.js file
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
