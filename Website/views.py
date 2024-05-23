6e241b77d39c92c3fecf4e89049018866afae261
WiEFv1 <w13f2406@gmail.com>
2024-05-24 05:29:10 +0800

    Added add task and edit task

--- Website/views.py
+++ Website/views.py
@@ -11,7 +11,7 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
+    if request.method == 'POST':
         if 'add_note' in request.form:
             note = request.form.get('note')  # Gets the note from the HTML
             if len(note) < 1:
@@ -27,14 +27,29 @@ def home():
             db.session.add(new_projecttable)
             db.session.commit()
             flash('Table Added!', category='success')
-            
+        
         if 'add_task' in request.form:
-            project_id = request.form.get('add_task')  # Gets the project ID from the button data attribute
-            new_task = Task(user_id=current_user.id, task_name="Untitled", task_status="Unknown", deadline="Unknown", description="Unknown", projecttable_id=project_id)
+            projecttable_id = request.form.get('projecttable_id')
+            new_task = Task(
+                user_id=current_user.id,
+                projecttable_id=projecttable_id,
+                task_name=request.form.get('task_name'),
+                task_status=request.form.get('task_status'),
+                deadline=request.form.get('deadline'),
+                description=request.form.get('description')
+            )
             db.session.add(new_task)
             db.session.commit()
             flash('Task Added!', category='success')
         
+        if 'delete_task' in request.form:
+            task_id = request.form.get('task_id')
+            task = Task.query.get(task_id)
+            if task and task.user_id == current_user.id:
+                db.session.delete(task)
+                db.session.commit()
+                flash('Task Deleted!', category='success')
+                
         if 'edit_task' in request.form:
             task_id = request.form.get('task_id')
             task = Task.query.get(task_id)
@@ -45,8 +60,9 @@ def home():
                 task.description = request.form.get('description')
                 db.session.commit()
                 flash('Task Updated!', category='success')
-        
+    
     return render_template("home.html", user=current_user)
+
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
@@ -59,3 +75,7 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+@views.route('/task-management')
+def mainpage():
+    return render_template("taskmanagementweb.html", user=current_user)
